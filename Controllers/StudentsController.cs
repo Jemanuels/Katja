512fe8e7a7939458d9feeb86154d30cd64ffadf5
Jurgen Emanuels <Jurgen.Emanuels@CyberIAM.com>
2020-12-14 17:03:50 +0200

    Add project files.

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,75 @@
+﻿using Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Katja.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly ApplicationContext _context;
+
+        public StudentsController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            // var students = _context.Students.AsNoTracking().Where(s => s.Age > 25).ToList();
+            //var students = _context.Students
+            //    .Include(e => e.Evaluations)
+            //    .Include(ss => ss.StudentSubjects)
+            //    .ThenInclude(s => s.Subject)
+            //    .FirstOrDefault();
+
+            //var students = _context.Students.FirstOrDefault();
+
+            //_context.Entry(students).Collection(e => e.Evaluations).Load();
+
+            //_context.Entry(students).Collection(ss => ss.StudentSubjects).Load();
+
+            //foreach (var studentSubject in students.StudentSubjects)
+            //{
+            //    _context.Entry(studentSubject).Reference(s => s.Subject).Load();
+            //}
+
+            //var evaluationsCount = _context.Entry(students).Collection(e => e.Evaluations).Query().Count();
+
+            //var gradesPerStudent = _context.Entry(students).Collection(e => e.Evaluations).Query().Select(e => e.Grade).ToList();
+
+            //students.LocalCalculation = evaluationsCount;
+
+            //var students = _context.Students.Select(s => new
+            //{
+            //    s.Name,
+            //    s.Age,
+            //    NumberOfEvaluations = s.Evaluations.Count
+            //}).ToList();
+
+            //var students = _context.Students.Where(s => s.Name.Equals("Jurgen Emanuels")).Select(s => new
+            //{
+            //    s.Name,
+            //    s.Age,
+            //    Explanations = string.Join(",", s.Evaluations.Select(e => e.AdditionalExplanation))
+            //}).FirstOrDefault();
+
+            //var students = _context.Students.FromSqlRaw(@"SELECT * FROM Student WHERE Name = {0}", "Jurgen Emanuels")
+            //    .Include(e => e.Evaluations)
+            //    .FirstOrDefault();
+
+            //return Ok(students);
+
+            var rowsAffected = _context.Database.ExecuteSqlRaw(@"UPDATE Student SET Age = {0} WHERE Name = {1}", 28, "Jurgen Emanuels");
+
+            return Ok(new { rowsAffected = rowsAffected });
+        }
+    }
+}
---
+++ Migrations/20201214131841_SeedAllData.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Katja.Migrations
+{
+    public partial class SeedAllData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Student",
+                columns: table => new
+                {
+                    StudentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: true),
+                    IsRegularStudent = table.Column<bool>(type: "bit", nullable: false, defaultValue: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.StudentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subject",
+                columns: table => new
+                {
+                    SubjectId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SubjectName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subject", x => x.SubjectId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Evaluation",
+                columns: table => new
+                {
+                    EvaluationId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Grade = table.Column<int>(type: "int", nullable: false),
+                    AdditionalExplanation = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StudentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Evaluation", x => x.EvaluationId);
+                    table.ForeignKey(
+                        name: "FK_Evaluation_Student_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Student",
+                        principalColumn: "StudentId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StudentDetails",
+                columns: table => new
+                {
+                    StudentDetailsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AdditionalInformation = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StudentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentDetails", x => x.StudentDetailsId);
+                    table.ForeignKey(
+                        name: "FK_StudentDetails_Student_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Student",
+                        principalColumn: "StudentId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StudentSubject",
+                columns: table => new
+                {
+                    StudentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SubjectId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentSubject", x => new { x.StudentId, x.SubjectId });
+                    table.ForeignKey(
+                        name: "FK_StudentSubject_Student_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Student",
+                        principalColumn: "StudentId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StudentSubject_Subject_SubjectId",
+                        column: x => x.SubjectId,
+                        principalTable: "Subject",
+                        principalColumn: "SubjectId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Student",
+                columns: new[] { "StudentId", "Age", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("660ed4cd-1361-4216-9faa-9636e4df681a"), 39, "Jurgen Emanuels" },
+                    { new Guid("410c14e3-e6df-45b8-8c6f-1e19aed675ac"), 58, "Alice Emanuels" },
+                    { new Guid("4addc421-0937-45cb-b55c-200b45c6caca"), 42, "Marsika Emanuels" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Subject",
+                columns: new[] { "SubjectId", "SubjectName" },
+                values: new object[,]
+                {
+                    { new Guid("7e69e207-5131-4791-9064-57f6d3c47fc8"), "Math" },
+                    { new Guid("89fc9e5d-74f6-4d2e-ae82-76f2b1decce7"), "English" },
+                    { new Guid("9e5f12c2-0aa2-49b0-9db2-7df40fecf9ad"), "History" },
+                    { new Guid("fee204f4-a51d-44bb-a3d7-dcc2b5ee5d4f"), "Science" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Evaluation",
+                columns: new[] { "EvaluationId", "AdditionalExplanation", "Grade", "StudentId" },
+                values: new object[,]
+                {
+                    { new Guid("3f6bde30-d3ed-4406-b160-079729d7adb4"), "First test...", 5, new Guid("660ed4cd-1361-4216-9faa-9636e4df681a") },
+                    { new Guid("e999d154-d9a9-47ea-88cb-2418b79998ba"), "Second test...", 4, new Guid("660ed4cd-1361-4216-9faa-9636e4df681a") },
+                    { new Guid("2b9cc46b-8c5e-4ac5-ae44-8bec7bceb139"), "First test...", 3, new Guid("410c14e3-e6df-45b8-8c6f-1e19aed675ac") },
+                    { new Guid("e4aa3d04-6cda-47d6-8101-5fc74647d28e"), "Last test...", 2, new Guid("410c14e3-e6df-45b8-8c6f-1e19aed675ac") },
+                    { new Guid("2091a967-d859-45bb-bd59-f768ff4adbdd"), "First test...", 5, new Guid("4addc421-0937-45cb-b55c-200b45c6caca") },
+                    { new Guid("64ccfdf2-37a9-420b-bea8-154b3e6ec65a"), "Last test...", 3, new Guid("4addc421-0937-45cb-b55c-200b45c6caca") }
+                });
+
+            migrationBuilder.InsertData(
+                table: "StudentSubject",
+                columns: new[] { "StudentId", "SubjectId" },
+                values: new object[,]
+                {
+                    { new Guid("660ed4cd-1361-4216-9faa-9636e4df681a"), new Guid("7e69e207-5131-4791-9064-57f6d3c47fc8") },
+                    { new Guid("410c14e3-e6df-45b8-8c6f-1e19aed675ac"), new Guid("7e69e207-5131-4791-9064-57f6d3c47fc8") },
+                    { new Guid("660ed4cd-1361-4216-9faa-9636e4df681a"), new Guid("89fc9e5d-74f6-4d2e-ae82-76f2b1decce7") },
+                    { new Guid("660ed4cd-1361-4216-9faa-9636e4df681a"), new Guid("9e5f12c2-0aa2-49b0-9db2-7df40fecf9ad") },
+                    { new Guid("410c14e3-e6df-45b8-8c6f-1e19aed675ac"), new Guid("fee204f4-a51d-44bb-a3d7-dcc2b5ee5d4f") },
+                    { new Guid("4addc421-0937-45cb-b55c-200b45c6caca"), new Guid("fee204f4-a51d-44bb-a3d7-dcc2b5ee5d4f") }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Evaluation_StudentId",
+                table: "Evaluation",
+                column: "StudentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentDetails_StudentId",
+                table: "StudentDetails",
+                column: "StudentId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentSubject_SubjectId",
+                table: "StudentSubject",
+                column: "SubjectId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Evaluation");
+
+            migrationBuilder.DropTable(
+                name: "StudentDetails");
+
+            migrationBuilder.DropTable(
+                name: "StudentSubject");
+
+            migrationBuilder.DropTable(
+                name: "Student");
+
+            migrationBuilder.DropTable(
+                name: "Subject");
+        }
+    }
+}
